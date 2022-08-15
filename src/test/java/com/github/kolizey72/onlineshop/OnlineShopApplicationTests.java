689f5bfdc3a7422671a643f139204dc45aea0f27
Valera Drokin <drokin.valera@gmail.com>
2022-08-16 01:59:20 +0300

    Init project

---
+++ src/test/java/com/github/kolizey72/onlineshop/OnlineShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kolizey72.onlineshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
